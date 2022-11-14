2b6114470201319bac611f851d824cbe2d1d9819
dohyeonj <jdh7693@naver.com>
2022-11-14 21:30:05 +0900

    first commit

---
+++ src/main/java/com/restapiform/RestApiFormApplication.java
@@ -0,0 +1,13 @@
+package com.restapiform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiFormApplication.class, args);
+    }
+
+}
